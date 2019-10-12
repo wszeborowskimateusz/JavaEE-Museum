f41e950c4bc4fb958a290e0dfdd46e4bb32d38d2
Mateusz <wszeborowskimateusz@gmail.com>
2019-10-12 15:09:27 +0200

    Initial commit

---
+++ src/main/java/pl/wszeborowski/mateusz/view/Config.java
@@ -0,0 +1,14 @@
+package pl.wszeborowski.mateusz.view;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ * Global config for JSF.
+ *
+ * @author wszeborowskimateusz
+ */
+@FacesConfig
+@ApplicationScoped
+public class Config {
+}
